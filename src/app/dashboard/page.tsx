4a2dbc6c0176f025542acf408cbf691186a0eac5
Shane <142130007+dustytrailsdev@users.noreply.github.com>
2026-03-17 18:23:17 -0500

    Feature: Search page (#17)
    * Feature: Search page
    
    * break searchbar into its own component, tone done styling
    
    * Feature: Search page 2
    
    ---------
    
    Co-authored-by: Dominic <dominicmcdev@gmail.com>

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,10 @@
 import BrowseCategories from "@/app/dashboard/_components/BrowseCategories";
+import SearchBar from "@/app/dashboard/_components/SearchBar";
 
 export default function DashboardPage() {
   return (
     <div>
+      <SearchBar />
       <BrowseCategories />
     </div>
   );
